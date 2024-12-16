b4c39e42fd195573656b98fef30f2f4a378091ea
MCC Loader <prod.kuyo@proton.me>
2024-12-16 22:25:03 +0700

    Update __init__.py
    fixed imports (reduce file size)

--- core/__init__.py
+++ core/__init__.py
@@ -1,34 +1,17 @@
 import os
 import sys
 import time
-import json
 import re
-import random
-import ctypes
-import shutil
-import platform
-import hashlib
 import io
 import requests
-import winsound
 import msvcrt
-import pyautogui
 import colorama
-import numpy as np
 import subprocess
-from random import choice, randint, shuffle
 from time import sleep, strftime
-from ctypes import WinDLL
 from colorama import Fore, init
-from faker import Faker
-from PIL import Image
-from clint.textui import progress
-from pypresence import Presence
 from requests import session
-from mss import mss as mss_module
 import webbrowser
 from os.path import isfile
-from cryptography.fernet import Fernet
 from pystyle import Box, Center, Write, Colors, Colorate, Add, Anime, System
 
 
